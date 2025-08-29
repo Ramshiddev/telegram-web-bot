767519fcf168981d03fc69981a34e8450de72165
Ramshiddev <ramshidbekrahimjonov@gmail.com>
2025-08-29 13:40:39 +0500

    push

---
+++ src/constants/db.js
@@ -0,0 +1,60 @@
+export function getData() {
+  return [
+    {
+      title: 'JavaScript',
+      price: 17.99,
+      Image:
+        'https://i.pinimg.com/1200x/6f/19/d9/6f19d9fe0feb113aec55c84ca14577b7.jpg',
+      id: 1,
+    },
+    {
+      title: 'ReactJS',
+      price: 15,
+      Image:
+        'https://i.pinimg.com/736x/bf/2d/4b/bf2d4bfb3d01310a40d631ebdb838d5f.jpg',
+      id: 2,
+    },
+    {
+      title: 'VueJS',
+      price: 13.5,
+      Image:
+        'https://i.pinimg.com/1200x/60/17/da/6017da3ed8f203fe979b16dae1ad2259.jpg',
+      id: 3,
+    },
+    {
+      title: 'NodeJS',
+      price: 13.99,
+      Image:
+        'https://i.pinimg.com/736x/86/32/c7/8632c75a281a61b786a600c303509cd0.jpg',
+      id: 4,
+    },
+    {
+      title: 'NestJS',
+      price: 12.5,
+      Image:
+        'https://cdn.dribbble.com/userupload/41930880/file/original-633d9b239c12bbb0788b9faf25058c54.png?resize=400x300',
+      id: 5,
+    },
+    {
+      title: 'NextJS',
+      price: 12.99,
+      Image:
+        'https://mobisoftinfotech.com/resources/wp-content/uploads/2022/04/next-JS-framework.png',
+      id: 6,
+    },
+    {
+      title: 'TypeScript',
+      price: 22.99,
+      Image:
+        'https://cdn.sanity.io/images/z9io1e0u/production/ef940e5fc27a64a1409e890789b01b87a1ac4bb7-1920x1080.png',
+      id: 7,
+    },
+    {
+      title: 'TensorflowJS',
+      price: 28.99,
+      Image:
+        'https://miro.medium.com/v2/resize:fit:690/1*9ZLdnWAhZBOkFPJ-qjDY4Q.png',
+      id: 8,
+    },
+  ];
+}
